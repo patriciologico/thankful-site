fc21594ca58a991716a540230b7f6d4b9e86d87b
csalinasval <csalinasval@gmail.com>
2014-10-06 02:03:03 -0400

    changed port again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express  = require('express');
 var compression = require('compression');
 var app      = express();
-var port     = process.env.PORT || 80;
+var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash 	 = require('connect-flash');
